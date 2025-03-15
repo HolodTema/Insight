3f6e65d959950d5458d2f30703affb79e2b80c2d
HolodTema <art.holodilov@gmail.com>
2025-03-15 16:35:07 +0300

    Not working attempt to get CPU usage in percents via /proc/stat file parsing in runtime.

--- app/src/main/java/com/terabyte/insight/util/CpuDetailsHelper.kt
+++ app/src/main/java/com/terabyte/insight/util/CpuDetailsHelper.kt
@@ -1,8 +1,17 @@
 package com.terabyte.insight.util
 
 import android.os.Build
+import android.util.Log
 import com.terabyte.insight.TEXT_NO_INFORMATION
 import com.terabyte.insight.model.DeviceDetail
+import com.terabyte.insight.util.CpuDetailsHelper.getAmountCores
+import com.terabyte.insight.util.CpuDetailsHelper.getCpuAbi
+import com.terabyte.insight.util.CpuDetailsHelper.getCpuArch
+import com.terabyte.insight.util.CpuDetailsHelper.getCpuLoadPercents
+import com.terabyte.insight.util.CpuDetailsHelper.getCpuModel
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 
 object CpuDetailsHelper {
 
@@ -22,6 +31,18 @@ object CpuDetailsHelper {
         )
     }
 
+    fun getCpuAbi(): DeviceDetail {
+        var value = ""
+        Build.SUPPORTED_ABIS.forEach { it ->
+            value += "$it\n"
+        }
+        return DeviceDetail(
+            "CPU ABI",
+            value,
+            "Application binary interfaces (ABI) that are supported by CPU"
+        )
+    }
+
     fun getAmountCores(): DeviceDetail {
         return DeviceDetail(
             "Amount of CPU cores",
@@ -30,11 +51,60 @@ object CpuDetailsHelper {
         )
     }
 
+    fun getCpuLoadPercents(): DeviceDetail {
+
+        var line = TEXT_NO_INFORMATION
+        try {
+            val process = Runtime.getRuntime()
+                .exec("grep 'cpu ' /proc/stat | awk '{usage=(\$2+\$4)*100/(\$2+\$4+\$5)} END {print usage \"%\"}'")
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            line = reader.readLine()
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return DeviceDetail(
+            "Cpu usage",
+            line,
+            "Processor usage in percents. Not supported after Android 11."
+        )
+    }
+
+
+    fun getCpuDetailedInfo(): DeviceDetail {
+        var value = TEXT_NO_INFORMATION
+        try {
+            val commands = arrayOf("/system/bin/cat", "/proc/cpuinfo")
+            val processBuilder = ProcessBuilder(*commands)
+            val process = processBuilder.start()
+            val inputStream = process.inputStream
+            val bytes = ByteArray(1024)
+            value = ""
+            while (inputStream.read(bytes) != -1) {
+                value += String(bytes)
+            }
+            inputStream.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (value.isEmpty()) {
+                value = TEXT_NO_INFORMATION
+            }
+        }
+        return DeviceDetail(
+            "Detailed CPU info",
+            value,
+            "CPU info from file /proc/cpuinfo"
+        )
+    }
+
     fun getAll(): ArrayList<DeviceDetail> {
         return arrayListOf(
             getCpuModel(),
             getCpuArch(),
-            getAmountCores()
+            getCpuAbi(),
+            getAmountCores(),
+            getCpuLoadPercents(),
+            getCpuDetailedInfo()
         )
     }
 }
